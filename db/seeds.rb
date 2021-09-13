3c2d80eab72b13adf760fb6280cfb5ca68712e79
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-13 17:25:42 -0400

    Seed comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,9 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+# vinman = Developer.create(name: "Vincent Orsini",github_profile_link: "https://github.com/Vinmanftw")
+# example = Game.create(title:"VietnamFrogger2.0",github_link: "",preview_href: "",description: "Get harris to the helicopter and successful exfil from Vietnam.",language: "C#",has_comments?: false,theme: "Vietnam War",game_type: "Action/Arcade",file_size: 0,keyboard?: false,controller?: true,developer_id: vinman.id)
+
+#create each feature
+
 puts "✅ Done seeding!"
