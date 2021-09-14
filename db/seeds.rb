384394bf743ac25124f4ae2bb57790b69b4b0cf4
Vincent <vao2002@gmail.com>
2021-09-14 17:28:00 -0500

    users must now input their youtube video id

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ GameFeature.destroy_all
 vinman = Developer.create(name: "Vincent Orsini",github_profile: "https://github.com/Vinmanftw")
 
 #games
-vietnam_frogger2 = Game.create(title:"VietnamFrogger2.0",github_link: "https://github.com/Vinmanftw/Vietnam_Frogger2.0",preview_href: "https://www.youtube.com/watch?v=L9s-Uaz8lLw",description: "Get harris to the helicopter and successful exfil from Vietnam.",language: "C#",has_comments?: false,theme: "Vietnam War",game_type: "Action/Arcade",file_size: 0,keyboard?: true,controller?: true,developer_id: vinman.id)
-breakout = Game.create(title:"Breakout", github_link: "https://github.com/Vinmanftw/Breakout-Game",preview_href: "https://www.youtube.com/watch?v=rzkqJHEb2bs",description: "Breakout remade with multiple space themed backgrounds as well as easter egg themed backgrouds. Get points for taking out targets and beating your high scores. Use points to buy different skins for either the paddle, ball, or targets!!",language: "C#",has_comments?: true,theme: "Breakout",game_type: "Arcade",file_size: 0,keyboard?: false,controller?: true,developer_id: vinman.id)
+vietnam_frogger2 = Game.create(title:"VietnamFrogger2.0",github_link: "https://github.com/Vinmanftw/Vietnam_Frogger2.0",preview_href: "L9s-Uaz8lLw",description: "Get harris to the helicopter and successful exfil from Vietnam.",language: "C#",has_comments?: false,theme: "Vietnam War",game_type: "Action/Arcade",file_size: 0,keyboard?: true,controller?: true,developer_id: vinman.id)
+breakout = Game.create(title:"Breakout", github_link: "https://github.com/Vinmanftw/Breakout-Game",preview_href: "rzkqJHEb2bs",description: "Breakout remade with multiple space themed backgrounds as well as easter egg themed backgrouds. Get points for taking out targets and beating your high scores. Use points to buy different skins for either the paddle, ball, or targets!!",language: "C#",has_comments?: true,theme: "Breakout",game_type: "Arcade",file_size: 0,keyboard?: false,controller?: true,developer_id: vinman.id)
 
 #create each feature
 
